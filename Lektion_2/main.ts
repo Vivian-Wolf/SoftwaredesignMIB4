9bac4ac832ea3d2caa9110739d95dbc839b695ed
Vivian Wolf <vivian.wolf@outlook.de>
2020-05-03 10:20:02 +0200

    Update main.ts

--- Lektion_2/main.ts
+++ Lektion_2/main.ts
@@ -1,4 +1,17 @@
 namespace Lektion_2 {
-    console.log("Hello World!");
 
+    // Lektion 2 Übungen
+    // Deklaration und Definiton von Variablen
+        //Variablen werden am Anfang klein geschreiben, keine Lehrzeichen, keine Sonderzeichen, außer $ und _ , darf nicht mit Zahl anfangen
+    let newBoolean: boolean = false;
+    let anyKindOfNumber: number;
+    let thisIsAString: string = "Hello World";
+    let sentence: string;
+
+    // mit ${variable} können Variablen direkt verwendet werden ohne ein + dazwischen schreiben zu müssen
+    anyKindOfNumber = 3;
+    sentence = "This is a example for ${thisIsAString}. The counter is set to ${sentence}";
+    console.log(sentence);
+    thisIsAString = "Hello again" + anyKindOfNumber;
+    console.log(thisIsAString); 
 }

4bc4c93a9bf2b0418a2712344c1c22f41f5a7b31
Vivian Wolf <vivian.wolf@outlook.de>
2020-07-16 17:56:10 +0200

    Create new Repository

--- Abschlussarbeit/Code/Person.ts
+++
@@ -1,9 +0,0 @@
-namespace Abschluss {
-    export class Person {
-        public name: String;
-        public lifepoints: number;
-        canBeAttacked: boolean;
-        posX: number = 0;
-        posY: number = 0;
-    }
-}
--- Abschlussarbeit/Code/Test/main.ts
+++
@@ -1,5 +0,0 @@
-function tosy1(): void {
-    let userInput: string = (<HTMLInputElement>document.getElementById("userInput")).value;
-    document.getElementById("mainInfo").innerHTML = "You wrote: " + userInput;
-    console.log(userInput);
-}
